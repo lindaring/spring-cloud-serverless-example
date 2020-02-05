d3b95579d05a648a9924ee51c88b5b67db489dab
linda simelane <linda@bbd.co.za>
2020-02-05 10:02:55 +0200

    Added cloud function example

---
+++ src/test/java/com/lindaring/serveless/SpringCloudServerlessExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lindaring.serveless;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServerlessExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
